bbb4cd19ccf9a0d2f3dc2477c7c0e5695f7d56e0
Xubiod <pitts.davis3@hotmail.com>
2019-01-15 17:44:38 -0500

    Update MainWindow.xaml.cs

--- steam-utility/MainWindow.xaml.cs
+++ steam-utility/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
